245ffa368cd38102db44a10e4ee882b2700ce91f
Guilherme Araujo <guilherme.araujo14@hotmail.com>
2024-04-29 17:07:12 -0300

    mudança no index.ts

--- src/api/core/index.ts
+++ src/api/core/index.ts
@@ -3,7 +3,7 @@ import router from "../../routes";
 import conexao from "../../database";
 import 'dotenv/config';
 import cors from 'cors';
-import Serverless from "serverless-http";
+// import Serverless from "serverless-http";
 
 
 const app = express();
@@ -13,13 +13,12 @@ const PORT = 3300;
 app.use(router)
 conexao();
 
-const servlessApp = Serverless(app);
+// const servlessApp = Serverless(app);
 
-export const handler = async (event: any, context: any) => {
-    return await servlessApp(event, context);
-  };
-/*
+// export const handler = async (event: any, context: any) => {
+//     return await servlessApp(event, context);
+//   };
+
 app.listen(PORT, ()=>{
     console.log(`Servidor funcionando na porta ${PORT}`);
 })
-*/
