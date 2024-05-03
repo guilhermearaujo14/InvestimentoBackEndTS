68a23a82d1cedeee3aad96d0f56a5e4aa0f958a2
guilherme.araujo14 <guilherme.araujo14@hotmail.com>
2024-05-03 08:04:15 -0300

    AJUSTE

--- src/services/movimentacoesServices/PesquisaMovimentacao/index.ts
+++ src/services/movimentacoesServices/PesquisaMovimentacao/index.ts
@@ -1,12 +1,12 @@
 import db from '../../../database';
 import Movimentacoes from '../../../models/movimentacaoModel';
 
-async function PesquisaMovimentacoes(USUARIO_ID: number, dataInicio?: string | undefined, dataFinal?: string | undefined, PAPEL?: string | undefined, TIPO_ATIVO_ID?: string){
+async function PesquisaMovimentacoes(USUARIO_ID: number, dataInicio?: string | undefined, dataFinal?: string | undefined, PAPEL?: string | undefined, TIPO_ATIVO_ID?: string, MOVIMENTACAO_ID?: string){
     const con = await db();
     try {
-        const sql = await Movimentacoes.PesquisaMovimentacao(USUARIO_ID, dataInicio, dataFinal, PAPEL, TIPO_ATIVO_ID);
+        const sql = await Movimentacoes.PesquisaMovimentacao(USUARIO_ID, dataInicio, dataFinal, PAPEL, TIPO_ATIVO_ID, MOVIMENTACAO_ID);
         const response: any = await con?.execute(sql)
-
+        console.log(response[0])
         return response[0]
 
     } catch (error) {
