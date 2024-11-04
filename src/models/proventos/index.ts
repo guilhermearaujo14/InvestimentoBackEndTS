1841cacdd984b6cfb65db780afeea2551df177d9
Guilherme Araujo <guilherme.araujo14@hotmail.com>
2024-11-04 19:55:10 -0300

    Corrige query para verificar se existe Provento pesquisando pelo papel.

--- src/models/proventos/index.ts
+++ src/models/proventos/index.ts
@@ -11,12 +11,17 @@ class Proventos{
         this.DATA_INCLUSAO = DATA_INCLUSAO
     }
 
-    static async CadastraProvento(USUARIO_ID: number, PAPEL: string, DATA_INCLUSAO: Date){
-        const sql = `INSERT INTO PROVENTOS VALUES (USUARIO_ID, PAPEL, DATA_INCLUSAO)
-        VALUES (${USUARIO_ID}, '${PAPEL}', ${DATA_INCLUSAO})`
+    static async CadastraProvento(USUARIO_ID: number, PAPEL: string){
+        const sql = `INSERT INTO PROVENTOS (USUARIO_ID, PAPEL, DATA_INCLUSAO)
+        VALUES (${USUARIO_ID}, '${PAPEL}', CURRENT_TIMESTAMP());`
         return sql;
     }
 
+
+    static async GetProventoByPapel(USUARIO_ID: number, PAPEL: string){
+        const sql = `SELECT * FROM PROVENTOS WHERE USUARIO_ID = ${USUARIO_ID} AND PAPEL = '${PAPEL}'`;
+        return sql;
+    }
 
 }
 
