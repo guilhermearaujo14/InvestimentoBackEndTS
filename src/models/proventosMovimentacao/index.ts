de3f9d6ea73e45c4304b542376a041c6ed59164d
Guilherme Araujo <guilherme.araujo14@hotmail.com>
2024-11-04 19:55:55 -0300

    Adiciona método de criar movimentacao

--- src/models/proventosMovimentacao/index.ts
+++ src/models/proventosMovimentacao/index.ts
@@ -12,6 +12,14 @@ class ProventosMovimentacao{
         this.DATA_PAGAMENTO = DATA_PAGAMENTO,
         this.DATA_INCLUSAO = DATA_INCLUSAO
     }
+
+    static async Create(PROVENTOS_ID: number, VALOR: number, DATA_PAGAMENTO: Date){
+        const sql = `INSERT INTO PROVENTOS_MOVIMENTACAO (PROVENTOS_ID, VALOR, DATA_PAGAMENTO, DATA_INCLUSAO) 
+        VALUES (${PROVENTOS_ID}, ${VALOR}, '${DATA_PAGAMENTO}', CURRENT_TIMESTAMP());`;
+        return sql
+    }
+
+
 }
 
 export default ProventosMovimentacao;
